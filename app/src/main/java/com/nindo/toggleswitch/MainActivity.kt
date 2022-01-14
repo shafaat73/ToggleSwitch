5e64d9befdde72f4ca5c0201e06f9925fc9c66c0
Shafaat <shafaat@phaedrasolutions.com>
2022-01-14 13:09:00 +0500

    init

--- app/src/main/java/com/nindo/toggleswitch/MainActivity.kt
+++ app/src/main/java/com/nindo/toggleswitch/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var view:ToggleSwitch=findViewById(R.id.customSwitch)
+        view.setSwitchClickListener {  }
     }
 }
